b532fbcbcbc608b8ba530c48f73c0642a9b9d342
praniteeharole <121255738+praniteeharole@users.noreply.github.com>
2024-10-19 14:08:04 +0530

    Delete JSON.py

--- JSON.py
+++
@@ -1,9 +0,0 @@
-import JSON
-empolyee_data =[{"fname":"abc", "lname":"xyz", "id": 1}, 
-                {"fname":"abc", "lname":"xyz", "id": 2}]
-                
-                
-            
-            
-JSON_data = json.dumps(employee_data, indent=2)
-print(json_data)        
